7d6d23c4489acf0fe90e49e337bacba1153ef191
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2023-05-09 22:56:48 +0200

    adding footer

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, abort, make_response, redirect, render_template, request
 
 load_dotenv()
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Handler for the application's root URL"""
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
