abcbc91194be4a0f1726cd74783ffca1c452de9c
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2023-05-09 22:33:32 +0200

    removing redundant files

--- admin/admin.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-
-
-adminpanel = Blueprint("adminpanel", __name__, template_folder="templates")
-
-
-@adminpanel.route("/")
-def index():
-    return render_template("adminpanel.html")
